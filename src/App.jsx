436667df0ae92589566381d1d350571b0d85c5dd
Manish <mjpatel84@gmail.com>
2023-11-12 16:56:56 -0500

    adds and imports router, and adds basic route setup to App

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/mercury", element: <Mercury /> },
+      { path: "/venus", element: <Venus /> },
+      { path: "/earth", element: <Earth /> },
+      { path: "/mars", element: <Mars /> },
+      { path: "/jupiter", element: <Jupiter /> },
+      { path: "/saturn", element: <Saturn /> },
+      { path: "/uranus", element: <Uranus /> },
+      { path: "/neptune", element: <Neptune /> },
+    ],
+  },
+]);
+
+export default function App() {
+  return <RouterProvider router={router} />;
+}
