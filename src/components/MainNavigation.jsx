e7ef4287108eb1b4995dc823768b570a52b14f41
Manish <mjpatel84@gmail.com>
2023-11-12 17:00:09 -0500

    adds MainNavigation file and basic structure

---
+++ src/components/MainNavigation.jsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+
+export default function MainNavigation() {
+  return (
+    <header>
+      <h2>The Planets</h2>
+      <nav>
+        <li>
+          <Link></Link>
+        </li>
+        <li>
+          <Link></Link>
+        </li>
+        <li>
+          <Link></Link>
+        </li>
+        <li>
+          <Link></Link>
+        </li>
+        <li>
+          <Link></Link>
+        </li>
+        <li>
+          <Link></Link>
+        </li>
+        <li>
+          <Link></Link>
+        </li>
+        <li>
+          <Link></Link>
+        </li>
+      </nav>
+    </header>
+  );
+}
